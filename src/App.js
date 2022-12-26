d6c25d31c44a1c064838d08a1f4a3cf36ed496cb
Selamet Saputra <keeita768900@gmail.com>
2022-12-26 19:50:28 +0700

    webfilmsearchapi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from './api';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    getMovieList().then((results) => {
+      setPopularMovies(results);
+    });
+  }, []);
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
+  const PopularMoviesList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className="Movie-date">{movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>emet webmania</h1>
+        <input placeholder="cari film kesayangan kamu..." className="Movie-search" onChange={({ target }) => search(target.value)} />
+        <div className="Movie-container">
+          <PopularMoviesList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
